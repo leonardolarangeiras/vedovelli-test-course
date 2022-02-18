4dea9375f37aba261d30e4ef91b8487423f86332
Leonardo Larangeiras <leonardoslarangeiras@gmail.com>
2022-02-18 16:29:19 -0300

    Ajustando umas coisas aqui

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString')
+const { queryString, parse } = require('./queryString')
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -31,5 +31,33 @@ describe('Object to query string', () => {
     }).toThrowError();
   })
 
+})
+
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Leonardo&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Leonardo',
+      profession: 'developer'
+    });
+  })
+
+  it('should convert a query string of a single key-value pair to object', () => {
+    const qs = 'name=Leonardo';
+
+    expect(parse(qs)).toEqual({
+      name: 'Leonardo',
+    });
+  })
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Leonardo&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Leonardo',
+      abilities: ['JS', 'TDD']
+    })
+  })
 })
 
