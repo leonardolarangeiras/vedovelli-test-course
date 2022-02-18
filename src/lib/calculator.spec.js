9eff8db02ad74c7d016ba31019a08d2a69d6b728
Leonardo Larangeiras <leonardoslarangeiras@gmail.com>
2022-02-18 16:44:48 -0300

    fiz alguma cagada aqui

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,4 +1,4 @@
-const {sum} = require('./calculator.js');
+import { sum } from './calculator.js';
 
 it('should sum 2 and 2 and the result must be 4', () => {
   expect(sum(2, 2)).toBe(4)
@@ -12,15 +12,15 @@ it('should throw an error if what is provided to the method connot be summed', (
   expect(() => {
     sum('', '2')
   }).toThrowError()
-  
+
   expect(() => {
     sum([2, 2])
   }).toThrowError()
-  
+
   expect(() => {
     sum({})
   }).toThrowError()
-  
+
   expect(() => {
     sum()
   }).toThrowError()
